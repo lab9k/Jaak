ad8a4eb8858c1232064f7d7440460435fa3d7ef8
wimdetr <wim.detroyer.w3755@student.hogent.be>
2017-05-11 18:06:23 +0200

    sqlite3

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,8 @@
 const Bot = require('./lib/Bot')
 const SOFA = require('sofa-js')
 const Fiat = require('./lib/Fiat')
-
 let bot = new Bot()
-// ROUTING
-
+    // ROUTING
 bot.onEvent = function (session, message) {
     switch (message.type) {
     case 'Init':
@@ -29,7 +27,8 @@ function onMessage(session, message) {
     if (message.body == `help`) {
         var hulp = "Gebruik volgende commando's om tegen de bot te praten \n" + "1. \'plaats\': geeft vrije plaatsen"
         sendMessage(session, hulp)
-    } else {
+    }
+    else {
         sendMessage(session, message.body)
     }
 }
@@ -45,15 +44,16 @@ function onCommand(session, command) {
     case 'donate':
         donate(session)
         break
+    case 'dbtonen':
+        toondb(session)
+        break
     }
 }
 
 function onPayment(session) {
     sendMessage(session, `Thanks for the payment! 🙏`)
 }
-
 // STATES
-
 function welcome(session) {
     sendMessage(session, `Hallo Wim!`)
 }
@@ -61,7 +61,6 @@ function welcome(session) {
 function pong(session) {
     sendMessage(session, `Pong`)
 }
-
 // example of how to store state on each user
 function count(session) {
     let count = (session.get('count') || 0) + 1
@@ -76,29 +75,44 @@ function donate(session) {
     })
 }
 
-// HELPERS
+function toondb(session) {
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database("parkingdb.db");
+    db.serialize(function () {
+            db.each("SELECT * FROM Test", function (err, row) {
+                console.log(row);
+            });
+        });
+        db.close();
+    }
 
+// HELPERS
 function sendMessage(session, message) {
     let controls = [
         {
-            type: 'button',
-            label: 'Toon parkeerplaatsen',
-            value: 'ping'
-        },
-        {
-            type: 'button',
-            label: 'Reserveer plekje',
-            value: 'count'
-        },
-        {
-            type: 'button',
-            label: 'Parkeerplaats afstaan',
-            value: 'donate'
+            type: 'button'
+            , label: 'Toon parkeerplaatsen'
+            , value: 'ping'
+        }
+        , {
+            type: 'button'
+            , label: 'Reserveer plekje'
+            , value: 'count'
+        }
+        , {
+            type: 'button'
+            , label: 'Parkeerplaats afstaan'
+            , value: 'donate'
+        }
+        , {
+            type: 'button'
+            , label: 'Toon db'
+            , value: 'dbtonen'
         }
   ]
     session.reply(SOFA.Message({
-        body: message,
-        controls: controls,
-        showKeyboard: false,
-    }))
+        body: message
+        , controls: controls
+        , showKeyboard: false
+    , }))
 }
